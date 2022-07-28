331a9b0558503f72c1a36452fcbf28397e6e2921
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-07-28 11:14:34 +0800

    :wrench: config(eslint): prevent eslint directive abuses

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:eslint-comments/recommended',
   ],
   root: true,
   env: {
